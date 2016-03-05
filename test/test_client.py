b39f021536e80760673e6e61655174421fc740ed
Harsh Vardhan <harshvd95@gmail.com>
2016-03-05 21:04:21 +0530

    adding after more tests

--- test/test_client.py
+++ test/test_client.py
@@ -11,12 +11,22 @@ def run():
 	person.id = int(raw_input("Enter your ID: "))
 	person.name = raw_input("Enter your name: ")
 	person.email = raw_input("Enter your email: ")
-	phone = person.phone.add()
-	phone.number = raw_input("Enter your phone-number: ")
-	phone.type = test_pb2.Person.HOME
+	for contact in xrange(3):
+		phone = person.phone.add()
+		if contact == 0:
+			phone.number = raw_input("Enter mobile-number: ")
+			phone.type = test_pb2.Person.MOBILE
+		elif contact == 1:
+			phone.number = raw_input("Enter home-number: ")
+			phone.type = test_pb2.Person.HOME
+		elif contact == 2:
+			phone.number = raw_input("Enter work-number: ")
+			phone.type = test_pb2.Person.WORK				
+
 	response = stub.get_addrs(test_pb2.Person(name=person.name), _TIMEOUT_SECONDS)
-	print "Test: " + response.person
-	print person
+	print "Test: " + response.person  		# Test: Hello, Harsh Vardhan!
+	print person           					
+	print len(person.phone)                 # Total phone-numbers added
 
 
 if __name__ == '__main__':
