67dcc5c4f32d00af54c01563098bee3ba27ad08c
Harsh Vardhan <harshvd95@gmail.com>
2016-03-05 22:00:12 +0530

    formatting code

--- test/test_client.py
+++ test/test_client.py
@@ -1,33 +1,38 @@
+
 from grpc.beta import implementations
 
 import test_pb2
 
 _TIMEOUT_SECONDS = 5
 
-def run():
-	channel = implementations.insecure_channel('localhost', 2000)
-	stub = test_pb2.beta_create_Address_stub(channel)
-	person = test_pb2.Person()
-	person.id = int(raw_input("Enter your ID: "))
-	person.name = raw_input("Enter your name: ")
-	person.email = raw_input("Enter your email: ")
-	for contact in xrange(3):
-		phone = person.phone.add()
-		if contact == 0:
-			phone.number = raw_input("Enter mobile-number: ")
-			phone.type = test_pb2.Person.MOBILE
-		elif contact == 1:
-			phone.number = raw_input("Enter home-number: ")
-			phone.type = test_pb2.Person.HOME
-		elif contact == 2:
-			phone.number = raw_input("Enter work-number: ")
-			phone.type = test_pb2.Person.WORK				
 
-	response = stub.get_addrs(test_pb2.Person(name=person.name), _TIMEOUT_SECONDS)
-	print "Test: " + response.person  		# Test: Hello, Harsh Vardhan!
-	print person           					
-	print len(person.phone)                 # Total phone-numbers added
+def run():
+    channel = implementations.insecure_channel('localhost', 2000)
+    stub = test_pb2.beta_create_Address_stub(channel)
+    person = test_pb2.Person()
+    person.id = int(raw_input('Enter your ID: '))
+    person.name = raw_input('Enter your name: ')
+    person.email = raw_input('Enter your email: ')
+    for contact in xrange(3):
+        phone = person.phone.add()
+        if contact == 0:
+            phone.number = raw_input('Enter mobile-number: ')
+            phone.type = test_pb2.Person.MOBILE
+        elif contact == 1:
+            phone.number = raw_input('Enter home-number: ')
+            phone.type = test_pb2.Person.HOME
+        elif contact == 2:
+            phone.number = raw_input('Enter work-number: ')
+            phone.type = test_pb2.Person.WORK
+
+    response = stub.get_addrs(test_pb2.Person(name=person.name),
+                              _TIMEOUT_SECONDS)
+    print 'Test: ' + response.person  # Test: Hello, Harsh Vardhan!
+    print person
+    print len(person.phone)  		  # Total phone-numbers added
 
 
 if __name__ == '__main__':
-	run()
+    run()
+
+			
--- test/test_server.py
+++ test/test_server.py
@@ -1,3 +1,4 @@
+
 import time
 
 import test_pb2
@@ -7,19 +8,22 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 class Address(test_pb2.BetaAddressServicer):
 
-  def get_addrs(self, request, context):
-    return test_pb2.AddressBook(person='Hello, %s!' % request.name)
+    def get_addrs(self, request, context):
+        return test_pb2.AddressBook(person='Hello, %s!' % request.name)
 
 
 def serve():
-  server = test_pb2.beta_create_Address_server(Address())
-  server.add_insecure_port('[::]:2000')
-  server.start()
-  try:
-    while True:
-      time.sleep(_ONE_DAY_IN_SECONDS)
-  except KeyboardInterrupt:
-    server.stop(0)
+    server = test_pb2.beta_create_Address_server(Address())
+    server.add_insecure_port('[::]:2000')
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
 
 if __name__ == '__main__':
-  serve()
+    serve()
+
+      
